472ad57ed81b239157618268d57f4c6474b3691d
Covadonga M <cova.millan@gmail.com>
2023-03-13 18:46:58 +0100

    Fixed sonar problem

--- src/db/db.connect.test.ts
+++ src/db/db.connect.test.ts
@@ -1,19 +1,19 @@
 import { dbConnect } from './db.connect';
 import mongoose from 'mongoose';
 describe('Given dbConnect function', () => {
-  describe('When NODE_ENV !== test', () => {
-    test('Then it should be a connection to testing db', async () => {
-      const result = await dbConnect('dev');
-      expect(typeof result).toBe(typeof mongoose);
-      expect(mongoose.connection.db.databaseName).not.toContain('Test');
-      mongoose.disconnect();
-    });
-  });
   describe('When NODE_ENV === test', () => {
     test('Then it should be a connection to testing db', async () => {
       const result = await dbConnect();
       expect(typeof result).toBe(typeof mongoose);
       expect(mongoose.connection.db.databaseName).toContain('Test');
+      mongoose.disconnect();
+    });
+  });
+  describe('When NODE_ENV !== test', () => {
+    test('Then it should be a connection to testing db', async () => {
+      const result = await dbConnect('dev');
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connection.db.databaseName).not.toContain('Test');
       mongoose.disconnect();
     });
   });
