f1b4fa6b1c06e905598e659d0ab27feb25b5417c
Covadonga M <cova.millan@gmail.com>
2023-03-13 18:43:04 +0100

    Add db.connect.ts file and test

---
+++ src/db/db.connect.ts
@@ -0,0 +1,15 @@
+import createDebug from 'debug';
+import mongoose from 'mongoose';
+import { config } from '../config.js';
+const debug = createDebug('Social:connect');
+
+const { user, passwd, cluster, dbName } = config;
+
+export const dbConnect = (env?: string) => {
+  const finalEnv = env || process.env.NODE_ENV;
+  const finalDBName = finalEnv === 'test' ? dbName + '_Testing' : dbName;
+
+  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
+  debug(uri);
+  return mongoose.connect(uri);
+};
