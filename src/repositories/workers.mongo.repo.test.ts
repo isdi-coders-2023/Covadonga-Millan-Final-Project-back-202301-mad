38ffba400ba82ba7aaa84b48541627f152cf9c1e
Covadonga M <cova.millan@gmail.com>
2023-03-14 19:17:36 +0100

    Add search method to test

--- src/repositories/workers.mongo.repo.test.ts
+++ src/repositories/workers.mongo.repo.test.ts
@@ -1,25 +1,31 @@
-import { WorkerModel } from './workers.mongo.model';
-import { WorkersMongoRepo } from './workers.mongo.repo';
+import { WorkerModel } from './workers.mongo.model.js';
+import { WorkersMongoRepo } from './workers.mongo.repo.js';
 
 jest.mock('./workers.mongo.model');
 
 describe('Given WorkersMongoRepo', () => {
   const repo = WorkersMongoRepo.getInstance();
-  const exec = jest.fn();
-  beforeEach(() => {
-    exec.mockResolvedValue([]);
-    WorkerModel.find = jest.fn().mockReturnValue({
-      populate: jest.fn().mockReturnValue({
-        exec,
-      }),
-    });
-  });
 
   describe('When it is instantiated', () => {
     test('Then it should be able to be instanced.', () => {
       expect(repo).toBeInstanceOf(WorkersMongoRepo);
     });
   });
+
+  describe('When we call the search() method', () => {
+    test('Then if it has an mock query object, it should return find resolved value', async () => {
+      const mockValue = [{ id: '1' }];
+      (WorkerModel.find as jest.Mock).mockResolvedValue(mockValue);
+
+      const mockTest = {
+        key: 'email',
+        value: 'cova.millan@jeje.com',
+      };
+      const result = await repo.search(mockTest);
+      expect(result).toEqual([{ id: '1' }]);
+    });
+  });
+
   describe('When we call the create() method with an empty object', () => {
     test('Then it should return an empty object.', async () => {
       (WorkerModel.create as jest.Mock).mockResolvedValue({});
