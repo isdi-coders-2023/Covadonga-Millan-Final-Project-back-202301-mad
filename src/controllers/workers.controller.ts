9c17bd843a63c7a5eaa0f3e32bda0d5149000c2d
Covadonga M <cova.millan@gmail.com>
2023-03-14 18:46:36 +0100

    Fixed worker controller

--- src/controllers/workers.controller.ts
+++ src/controllers/workers.controller.ts
@@ -1,30 +1,40 @@
 import createDebug from 'debug';
 import { Request, NextFunction, Response } from 'express';
-import { Worker } from '../entities/worker.js';
 import { HTTPError } from '../errors/httpError.js';
 import { Auth, PayloadToken } from '../helpers/auth.js';
-import { RepoWorker } from '../repositories/repoWorker.interface.js';
-import { WorkerModel } from '../repositories/workers.mongo.model.js';
-const debug = createDebug('pet:hospital:workers');
+import { WorkersMongoRepo } from '../repositories/workers.mongo.repo.js';
+const debug = createDebug('pet-hospital:workers');
 
 export class WorkersController {
-  constructor(public repo: RepoWorker<Worker>) {
+  constructor(public repo: WorkersMongoRepo) {
     debug('Instantiate');
   }
 
   async login(req: Request, resp: Response, next: NextFunction) {
     try {
+      // eslint-disable-next-line no-debugger
       debug('Login:post');
+      debug(req.body.email);
+      debug(req.body.password);
+
       if (!req.body.email || !req.body.password)
         throw new HTTPError(401, 'Unauthorized', 'Invalid email or password');
+
       const data = await this.repo.search({
         key: 'email',
         value: req.body.email,
       });
+
+      console.log(data, req.body);
+
       if (!data.length)
         throw new HTTPError(401, 'Unauthorized', 'Email not found');
-      if (!(await Auth.compare(req.body.password, data[0].password)))
+
+      // A if (!(await Auth.compare(req.body.password, data[0].password)))
+
+      if (req.body.password !== data[0].password)
         throw new HTTPError(401, 'Unauthorized', 'Password does not match');
+
       const payload: PayloadToken = {
         id: data[0].id,
         email: data[0].email,
