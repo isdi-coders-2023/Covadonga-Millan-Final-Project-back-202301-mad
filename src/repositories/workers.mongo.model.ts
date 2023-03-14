0b94568c2e9f3ebdcbf112d71d6d1d214124c38e
Covadonga M <cova.millan@gmail.com>
2023-03-14 10:45:39 +0100

    Add workers model

---
+++ src/repositories/workers.mongo.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+import { Worker } from '../entities/worker.js';
+
+const workerSchema = new Schema<Worker>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+export const WorkerModel = model('User', workerSchema, 'users');
