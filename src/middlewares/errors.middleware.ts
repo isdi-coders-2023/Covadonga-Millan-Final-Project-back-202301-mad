a88f039c2fd0a4a97c91bba826bf08a0b72ec060
Covadonga M <cova.millan@gmail.com>
2023-03-17 10:10:25 +0100

    Add errors middleware

---
+++ src/middlewares/errors.middleware.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { Error } from 'mongoose';
+import { CustomError, HTTPError } from '../errors/httpError';
+import createDebug from 'debug';
+const debug = createDebug('pet-hospital:errors middleware');
+
+export const errorsMiddleware = (
+  error: CustomError | Error,
+  _req: Request,
+  resp: Response
+) => {
+  debug('errors middleware');
+
+  let status = 500;
+  let statusMessage = 'Internal server error';
+
+  if (error instanceof HTTPError) {
+    status = error.statusCode;
+    statusMessage = error.statusMessage;
+  }
+
+  if (error instanceof Error.CastError) {
+    status = 400;
+    statusMessage = 'Bad formatted data in the request';
+  }
+
+  if (error instanceof Error.ValidationError) {
+    status = 406;
+    statusMessage = 'Validation error in the request';
+  }
+
+  resp.status(status);
+  resp.json({
+    error: [
+      {
+        status,
+        statusMessage,
+      },
+    ],
+  });
+  debug(status, statusMessage, error.message);
+};
