07453d096d0b52758f68975b13decba6ff6af358
Covadonga M <cova.millan@gmail.com>
2023-03-20 17:27:49 +0100

    Add pets router

--- src/routers/pets.router.ts
+++ src/routers/pets.router.ts
@@ -1,6 +1,8 @@
 /* eslint-disable new-cap */
 import createDebug from 'debug';
 import { Router } from 'express';
+import { PetsController } from '../controllers/pets/pets.controller';
+import { logged } from '../interceptors/logged';
 import { PetsMongoRepo } from '../repositories/pets/pets.mongo.repo';
 const debug = createDebug('pet-hospital:router:pets');
 
@@ -8,10 +10,11 @@ export const petsRouter = Router();
 debug('Loaded pets');
 
 const repo = new PetsMongoRepo();
-//  A const controller = new PetsController(repo)
-// petsRouter.post('/', logged, controller.post.bind(controller));
-// petsRouter.get('/', logged, controller.getAll.bind(controller));
-// petsRouter.patch('/discharge/:id', logged, controller.discharge.bind(controller));
-// petsRouter.get('/:id', logged, controller.get.bind(controller));
-// petsRouter.patch('/:id', logged, controller.patch.bind(controller));
-// petsRouter.delete('/:id', logged, controller.delete.bind(controller));
+const controller = new PetsController(repo);
+petsRouter.post('/', logged, controller.create.bind(controller));
+petsRouter.patch('/discharge/:id', logged, controller.update.bind(controller));
+petsRouter.patch('/:id', logged, controller.update.bind(controller));
+petsRouter.delete('/:id', logged, controller.delete.bind(controller));
+petsRouter.get('/owners/:owner', logged, controller.findOwner.bind(controller));
+petsRouter.get('/:id', logged, controller.find.bind(controller));
+petsRouter.get('/', logged, controller.query.bind(controller));
